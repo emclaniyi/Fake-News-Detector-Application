96b3fd143a2b45b5dc38d6e177fc332437928d44
Emerald Mclaniyi <emclaniyi@gmail.com>
2020-10-17 20:29:33 +0100

    predict function

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for, request
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import sqlite3
 
+#loading saved model
+pickle_in = open('', 'rb')
+model = pickle.load(pickle_in)
 
 app = Flask(__name__)
 
@@ -8,9 +13,18 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    return render_template("result.html")
+    cv = CountVectorizer()
+    if request.method == 'POST':
+        text = request.form['comment']
+        data = [text]
+        vect = cv.transform(data).toarray()
+        my_prediction = model.predict(vect)
+
+    return render_template("result.html", prediction=my_prediction)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
